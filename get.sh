b01f6abaa2a78bfc1a4f612cb591d1aa1c28355c
Hoàn Đặng <danghuyhoan@gmail.com>
2015-06-06 14:05:09 +1000

    Create get.sh

---
+++ get.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+git clone https://github.com/hoandang/wordpress-seed.git && cd wordpress-seed && rm -rf .git
+composer update && cd app && mkdir themes
+git clone https://github.com/Automattic/_s.git && rm -rf _s/.git && mv _s themes/
+cd themes/_s
+mkdir -p dev/styles
+mkdir -p dev/images
+mkdir -p dev/scripts
+mkdir scripts
+mkdir images
+mv ../../../package.json . && mv ../../../gulpfile.js . && mv ../../../bower.json .
+npm install && bower install && gulp build
+cd ../../../
